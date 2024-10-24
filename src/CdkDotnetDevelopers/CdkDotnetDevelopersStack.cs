e2ae39003ed7a512f6d046596e9ee7fe3f38e20f
Gençay Yıldız <gyildizmail@gmail.com>
2024-10-25 00:02:39 +0300

    Initial commit

---
+++ src/CdkDotnetDevelopers/CdkDotnetDevelopersStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CdkDotnetDevelopers
+{
+    public class CdkDotnetDevelopersStack : Stack
+    {
+        internal CdkDotnetDevelopersStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
